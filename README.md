536ca9f81cd742c01ab7c3de40515df8841f55b2
Vishwanath <vishwanathvishwabai@gmail.com>
2021-07-23 03:30:26 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align="center">Recipe App</h1>
+
+
+ RECIPE API --> https://www.edamam.com
+
+
+**Languages**
+```
+1.HTML
+2.CSS
+3.JAVASCRIPT
+```
+**Features**
+```
+1.Search Filter
+2.Responsive Design
+3.Animation
+```
+
+
+<hr>
+<p align="center">Thanks for coming, Keep coming ❤️.</p>
---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+
+const APP_ID = "c427fdd7";
+const APP_KEY = "15f8d86990cbfbe962aef02199af45f5";
+
+const form = document.getElementById("form");
+const searchBox = document.getElementById("searchBox");
+const main = document.getElementById("main");
+
+
+// FORM
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const filter = searchBox.value;
+
+    fetchData(filter);
+
+    searchBox.value = "";
+
+})
+
+
+// FETCH DATA
+
+function fetchData(data) {
+
+    const URL = `https://api.edamam.com/api/recipes/v2?type=public&q=${data}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    fetch(URL)
+    .then(res => res.json())
+    .then(datas => {
+
+        const hits = datas.hits;
+
+        getData(hits);
+    })
+}
+
+
+// DISPLAY DATA
+
+function getData(results){
+
+    let output = '';
+
+    results.map(result => {
+
+        let calories = result.recipe.calories;
+        let caloriesValue = Math.round(calories);
+
+
+        output += `
+            <div class="card__container">
+                <div class="box">
+                    <img src="${result.recipe.image}" alt="Recipe Image" class="recipe__img">
+                    <div class="info__box">
+                        <div class="info">
+                            <div class="name">${result.recipe.label}</div>
+                            <div class="calories">Calories: ${caloriesValue}</div>
+                        </div>
+                        <div class="view__link">
+                            <a href="${result.recipe.url}" target="_blank">View Recipe</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    })
+
+    main.innerHTML = output;
+}
